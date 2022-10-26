d756470735251d699de5c5fd9e416db5e311c3a5
Rudy Ajuchán <rudyajuchansec32@gmail.com>
2022-10-26 11:45:25 -0600

    modified:   Vista/encabezado.php 	modified:   Vista/pie.php 	deleted:    assets/css/estilo_sidebar.css 	new file:   assets/css/sidebar.css 	new file:   assets/img/wave_sidebar.png 	new file:   assets/js/sidebar.js

--- Vista/encabezado.php
+++ Vista/encabezado.php
@@ -4,135 +4,58 @@ if($_SESSION["AsisUMG_usuario"]=="" || $_SESSION["AsisUMG_usuario"]==null){
     header("Location: http://localhost:8070/AsistenciaUMG/");
 }
 ?>
-<!doctype html>
+<!DOCTYPE html>
 <html lang="en">
-  <head>
-  	<title>Asistencia UMG</title>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-    <link rel="stylesheet" href="https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css">
-		
-		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
-		<link rel="stylesheet" href="../assets/css/estilo_sidebar.css">
-  </head>
-  <body>
-		
-		<div class="wrapper d-flex align-items-stretch">
-			<nav id="sidebar">
-				<div class="p-4 pt-5">
-		  		<a href="#" class="img logo rounded-circle mb-5" style="background-image: url(../assets/img/umg_logo.png);"></a>
-	        <ul class="list-unstyled components mb-5">
-	          <li class="active">
-	            <a href="#homeSubmenu" data-toggle="collapse" aria-expanded="false" class="">Proyecto Asistencia</a>
-	          </li>
-
-            <li class="nav-item">
-                <a href="#"><i class="bx bxs-book-content"></i>
-                <span>Carrera</span></a>
-            </li>
-
-            <li class="nav-item">
-                <a href="#"><i class="bx bxs-school"></i>
-                <span>Centro</span></a>
-            </li>
-
-            <li class="nav-item">
-                <a href="#"><i class="bx bxs-notepad"></i>
-                <span>Cursos</span></a>
-            </li>
-
-
-
-
-             <li class="nav-item">
-                <a href="#homeSubmenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle"><i class='bx bx-edit-alt'></i>&nbsp;Secccion</a>
-                <ul class="collapse list-unstyled" id="homeSubmenu">
-                <li>
-                    <a href="#">Registrar Seccion</a>
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+    <link rel="stylesheet" href="../assets/css/sidebar.css">
+    <title>Asistencia UMG</title>
+</head>
+<body>
+    <header>
+        <nav>
+            <ul>
+                <li class="mb-4">
+                    <a href="#" class="logo"><img src="../assets/img/umg_logo.png" alt=""><span class="nav-item-custom text-white">UMG Asistencia</span></a>
                 </li>
-                <li>
-                    <a href="#">Asignar Carrera  -  Seccion</a>
-                </li>
-                <li>
-                    <a href="#">Asignar Seccion - curso</a>
-                </li>
-                </ul>
-              </li>
+                <li><a href="http://localhost:8070/AsistenciaUMG/Page/dashboard"><i class="fa-solid fa-house"></i><span class="nav-item-custom">Inicio</span></a></li>
+                <li><a href="#"><i class="fa-solid fa-school"></i><span class="nav-item-custom">Carrera</span></a></li>
+                <li><a href="#"><i class="fa-solid fa-building-columns"></i><span class="nav-item-custom">Centro</span></a></li>
+                <li><a href="#"><i class="fa-solid fa-graduation-cap"></i><span class="nav-item-custom">Cursos</span></a></li>
 
+                <li class="sub-btn"><a href="#"><i class="fa-solid fa-school-circle-check"></i><span class="nav-item-custom">Sección</span><i class="fa-solid fa-chevron-right"></i></a></li>
+                <div class="sub-menu">
+                    <ul>
+                        <li><a href="#" class="nav-item-custom"><i class="fa-solid fa-hand-point-right"></i>Registrar Sección</a></li>
+                        <li><a href="#" class="nav-item-custom"><i class="fa-solid fa-hand-point-right"></i>Asignar Carrera - Sección</a></li>
+                        <li><a href="#" class="nav-item-custom"><i class="fa-solid fa-hand-point-right"></i>Asignar Sección - curso</a></li>                        
+                    </ul>
+                </div>
+                
+                <li><a href="#"><i class="fa-solid fa-calendar-days"></i><span class="nav-item-custom">Horarios</span></a></li>
+                <li><a href="#"><i class="fa-solid fa-person-chalkboard"></i><span class="nav-item-custom">Docente</span></a></li>                
+                <li><a href="#"><i class="fa-solid fa-file-pen"></i><span class="nav-item-custom">Inscripción</span></a></li>
 
-            <li class="nav-item">
-                <a href="#"><i class="bx bxs-calendar"></i>
-                <span>Horarios</span></a>
-            </li>
-            <li class="nav-item">
-                <a href="#"><i class="bx bxs-user-badge"></i>
-                <span>Docente</span></a>
-            </li>
-            <li class="nav-item">
-                <a href="#"><i class="bx bxs-group"></i>
-                <span>Alumnos</span></a>
-            </li>
-            <li class="nav-item">
-                <a href="#"><i class="bx bxs-dollar-circle"></i>
-                <span>Inscripcion</span></a>
-            </li>
-
-
-
-
-
-
-
-            <li class="nav-item ">
-                <a href="#homeSubmenu2" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle"><i class='bx bxs-calendar' ></i>&nbsp;Asistencia</a>
-                <ul class="collapse list-unstyled" id="homeSubmenu2">
-                <li>
-                    <a href="#">Por Fecha - Estudiante</a>
-                </li>
-                <li>
-                    <a href="#">Por curso - Seccion - Fecha </a>
-                </li>
-                <li>
-                    <a href="#">Por semestre </a>
-                </li>
-
-                <li>
-                    <a href="#">Por carreras </a>
-                </li>
-
-
-                <li>
-                    <a href="#">Por centro </a>
-                </li>
-
-                <li>
-                    <a href="#">Todas las estadisticas </a>
-                </li>
-
-                </ul>
-              </li>
-
-
-
-
-	        </ul>
-
-	        <div class="footer">
-	        	<p><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-						  Copyright &copy;<script>document.write(new Date().getFullYear());</script> Todos los registros reservados | Grupo 10 Desarrollo web UMG
-						  <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></p>
-	        </div>
-
-	      </div>
-    	</nav>
-
-        <!-- Page Content  -->
-        <div class="container my-4">
-          <div class="row">
-            <div class="col-md-12">
-              <button type="button" id="sidebarCollapse" class="btn btn-primary">
-                <i class="fa fa-bars"></i>
-                <span class="sr-only">Toggle Menu</span>
-              </button><br>
+                <li class="sub-btn"><a href="#" ><i class="fa-solid fa-user-pen"></i><span class="nav-item-custom">Asistencia</span><i class="fa-solid fa-chevron-right"></i></a></li>
+                <div class="sub-menu">
+                    <ul>
+                        <li><a href="#" class="nav-item-custom"><i class="fa-solid fa-hand-point-right"></i> <span>Por Fecha - Estudiante</span></a></li>
+                        <li><a href="#" class="nav-item-custom"><i class="fa-solid fa-hand-point-right"></i> <span>Por curso - Sección - Fecha</span></a></li>
+                        <li><a href="#" class="nav-item-custom"><i class="fa-solid fa-hand-point-right"></i> <span>Por semestre</span></a></li>
+                        <li><a href="#" class="nav-item-custom"><i class="fa-solid fa-hand-point-right"></i> <span>Por Carreras</span></a></li>
+                        <li><a href="#" class="nav-item-custom"><i class="fa-solid fa-hand-point-right"></i> <span>Por Centro</span></a></li>
+                        <li><a href="#" class="nav-item-custom"><i class="fa-solid fa-hand-point-right"></i> <span>Todas las estadísticas</span></a></li>
+                    </ul>
+                </div>
+                
+                <li class="final"><a href="#"><i class="fa-solid fa-chalkboard-user"></i><span class="nav-item-custom">Alumnos</span></a></li>
+                <li><a href="#" class="logout"><i class="fa-solid fa-right-from-bracket"></i><span class="nav-item-custom sp-logout">Cerrar sesión</span></a></li>
+            </ul>
+        </nav>
+    </header>
+    
+    <div class="container my-4 content">
